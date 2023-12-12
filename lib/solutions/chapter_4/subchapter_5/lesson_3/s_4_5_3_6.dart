328e724bbc3e4eaf966dd8e0a298923406a55b68
Toni <toni95@users.noreply.github.com>
2023-12-12 13:43:14 +0100

    aufgaben erledigt

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -2,8 +2,41 @@ import 'package:flutter/material.dart';
 
 class S4536 extends StatelessWidget {
   const S4536({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final List<Product> products = [
+      Product(name: 'Apfel', price: 1.99),
+      Product(name: 'Brot', price: 2.50),
+      Product(name: 'Milch', price: 0.99),
+      Product(name: 'Käse', price: 3.49),
+      Product(name: 'Butter', price: 2.29),
+    ];
+    return ListView.builder(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.shopping_cart),
+          title: Text(products[index].name),
+          subtitle: Text('€${products[index].price.toStringAsFixed(2)}'),
+        );
+      },
+      shrinkWrap: true,
+    );
   }
 }
+
+class Product {
+  final String name;
+  final double price;
+
+  Product({required this.name, required this.price});
+}
+
+final List<Product> products = [
+  Product(name: 'Apfel', price: 1.99),
+  Product(name: 'Brot', price: 2.50),
+  Product(name: 'Milch', price: 0.99),
+  Product(name: 'Käse', price: 3.49),
+  Product(name: 'Butter', price: 2.29),
+];
