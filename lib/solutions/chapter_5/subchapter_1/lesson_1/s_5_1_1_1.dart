f6432997c70220fd7bbe73e2847aeadf2f55bf04
Toni <toni95@users.noreply.github.com>
2024-01-02 16:10:53 +0100

    aufgaben

--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_1.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_1.dart
@@ -4,6 +4,65 @@ class S5111 extends StatelessWidget {
   const S5111({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Quest5111();
+  }
+}
+
+class Quest5111 extends StatefulWidget {
+  const Quest5111({super.key});
+
+  @override
+  State<Quest5111> createState() => _Quest5111State();
+}
+
+class _Quest5111State extends State<Quest5111> {
+  bool isSwitched = false;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          const Text(
+            "Hello World!",
+            style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 35,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              ),
+              const SizedBox(
+                width: 50,
+              ),
+              Container(
+                color: Colors.green,
+                height: 100,
+                width: 100,
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Switch(
+            value: isSwitched,
+            onChanged: (value) {
+              setState(() {
+                isSwitched = value;
+                // You can also perform additional actions here when the switch is toggled
+              });
+            },
+            activeTrackColor: Colors.orange.shade200,
+            activeColor: Colors.orange,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
@@ -1,9 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class S5112 extends StatelessWidget {
   const S5112({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Quest5112();
   }
+}
+
+class Quest5112 extends StatelessWidget {
+  const Quest5112({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset("assets/images/Lama.png"),
+            const SizedBox(
+              height: 15,
+            ),
+            headline("Name"),
+            foodline("Diana das Lama"),
+            const SizedBox(
+              height: 15,
+            ),
+            headline("Gewicht"),
+            foodline("120kg"),
+            const SizedBox(
+              height: 15,
+            ),
+            headline("Größe"),
+            foodline("1,80m"),
+            const SizedBox(
+              height: 15,
+            ),
+            headline("Lieblingsessen"),
+            foodline("Gras"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget headline(String text) {
+  return Text(text,
+      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
+}
+
+Widget foodline(String text) {
+  return Text(text, style: const TextStyle(fontSize: 20));
 }
